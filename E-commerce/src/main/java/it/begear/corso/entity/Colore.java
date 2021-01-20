6712ff0e5940e96ed05a4525ca3a6ef057a6591c
irenerosaria <39551204+irenerosaria@users.noreply.github.com>
2021-01-20 13:53:18 +0100

    bu

--- E-commerce/src/main/java/it/begear/corso/entity/Colore.java
+++
@@ -1,5 +0,0 @@
-package it.begear.corso.entity;
-
-public enum Colore {
-	bianco,nero,blu,marrone,rosso;
-}
--- E-commerce/src/main/java/it/begear/corso/entity/Genere.java
+++
@@ -1,5 +0,0 @@
-package it.begear.corso.entity;
-
-public enum Genere {
-	uomo,donna,bambino,bambina;
-}
